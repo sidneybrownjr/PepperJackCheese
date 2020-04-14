7c1e3295d20840459789bfe4ad86b9f91ec3cfdd
bigmatt413 <55522208+bigmatt413@users.noreply.github.com>
2020-04-14 18:26:11 -0500

    update for like

--- bihtter/backend/routes/tweets.js
+++ bihtter/backend/routes/tweets.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 let Tweet = require('../models/bihtter.model');
 
+
 router.route('/').get((req, res) => {
     Tweet.find()
     .then(tweets => res.json(tweets))
@@ -10,12 +11,14 @@ router.route('/').get((req, res) => {
 router.route('/post').post((req, res) => {
     const username = req.body.username; 
     const description = req.body.description;
+    const like = req.body.like;
     const date = Date.parse(req.body.date);
 
     const newTweet = new Tweet({
         username,
         description,
-        date
+        like,
+        date,
     });
 
     newTweet.save()
@@ -23,4 +26,51 @@ router.route('/post').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route("/like").post((req,res,next) => {
+    const tweet_id = req.body.tweet_id;
+    const userID = req.body.user_id; 
+
+    Tweet.findByIdAndUpdate({
+        _id: tweet_id , 
+    },{ 
+        $addToSet: { like: userID }
+      }, null, (err, sessions) => {
+        if (err) {
+            return res.send({
+                success: false,
+                message: 'Error: Server Error'
+            });
+          }else{
+            return res.send({
+                success: true,
+                message: 'good'
+            });
+          }
+        
+        })
+    })
+router.route("/unlike").post((req,res,next) => {
+        const tweet_id = req.body.tweet_id;
+        const userID = req.body.user_id; 
+    
+        Tweet.findByIdAndUpdate({
+            _id: tweet_id , 
+        },{ 
+            $pull: { like: userID }
+          }, null, (err, sessions) => {
+            if (err) {
+                return res.send({
+                    success: false,
+                    message: 'Error: Server Error'
+                });
+              }else{
+                return res.send({
+                    success: true,
+                    message: 'good'
+                });
+              }
+            
+            })
+        })
+    
 module.exports = router;
