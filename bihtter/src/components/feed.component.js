58957ad92297007c22c48a9693d47f6f3fbdc905
bigmatt413 <55522208+bigmatt413@users.noreply.github.com>
2020-04-14 18:27:52 -0500

    update for like

--- bihtter/src/components/feed.component.js
+++ bihtter/src/components/feed.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './feed.css';
+import Like from "./like.component"
+import { getFromStorage } from "../storage";
 export default class Feed extends Component{
     constructor(props){
         super(props);
@@ -8,7 +10,6 @@ export default class Feed extends Component{
     this.state = {tweets:[]};
          
     }
-
     componentDidMount(){
         axios.get('http://localhost:5000/tweets/')
         .then(response => {
@@ -21,17 +22,25 @@ export default class Feed extends Component{
     }
     renderItems() {
         return this.state.tweets.map(tweets => (
-        <div class="solid" key={tweets._id}>
+        <div className="solid" key={tweets._id}>
             <p>UserName: {tweets.username}</p>
             <p>Description: {tweets.description}</p>
-            <p>Date: {tweets.date}</p>
+            <p>Date: {tweets.date.substring(0,10)}</p>
+            <p>Like:{tweets.like.length} 
+                <Like 
+                    tweet_id ={tweets._id} 
+                    like_arr ={tweets.like}
+                    user_id ={getFromStorage("user_id")}
+                />
+             </p>
           </div>
+
         ));
       }
     
     render(){
         return(
-            <div class ="soild">
+            <div className ="soild">
                 <h5>Feed:</h5>
                 <div>{this.renderItems()}</div>;
             </div>
